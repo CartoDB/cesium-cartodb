6655cc983b9486ac4bde71a7394ba47fc9c6d2d2
Dani Carrion <daniel@cartodb.com>
2015-02-16 21:04:39 +0100

    initial release

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/<%= pkg.name %>.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        },
+        "http-server": {
+            dev: {
+                root: ".",
+                port: 8000,
+                host: "127.0.0.1",
+                showDir: false,
+                autoIndex: false,
+                ext: "html",
+                runInBackground: false
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-http-server');
+    grunt.registerTask('default', ['http-server:dev']);
+    grunt.registerTask('build', ['uglify']);
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+# CartoDB integration for Cesium
